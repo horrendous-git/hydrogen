f69df915bb60bc0adb124d9bae227621a0dc9db0
Horrendous Git <horrendous.git@gmail.com>
2015-05-25 03:42:42 +0530

    rm gsl

--- src/test.cc
+++ src/test.cc
@@ -2,7 +2,7 @@
 
 
 #include <iostream>
-#include <gsl/gsl_sf_bessel.h>
+// #include <gsl/gsl_sf_bessel.h>
 
 #include "graph.hh"
 
@@ -11,9 +11,10 @@ using namespace std;
 int main() {
 	cout << "hello world" << endl;
 
-	double x = 5.0;
-	double y = gsl_sf_bessel_J0(x);
-	cout << "x: " << x << " y: " << y << endl;
+	// testing gsl
+	// double x = 5.0;
+	// double y = gsl_sf_bessel_J0(x);
+	// cout << "x: " << x << " y: " << y << endl;
 
 	Graph *g = new Graph("./graph1.txt");
 	delete(g);
